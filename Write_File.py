11b42bc150eea15bad6161ab75eaf091db366be1
ranjitdhepe <30865392+ranjitdhepe@users.noreply.github.com>
2017-09-02 16:48:26 +0530

    Write data to file with current data and time

--- Write_File.py
+++ Write_File.py
@@ -1,11 +1,11 @@
 import datetime
 import random
 i=0
-while i< 100 :
+while i< 1:
     f = open('C:\Python33\helloworld.csv', 'a')
     f.write('\n202284221,4.0.0,SDPICK,REQU_USERCONTEXT,162810,1,')
     f.write('{:%Y%m%d}'.format(datetime.datetime.now())) #20170504
-    f.write('{:,%H:%M:%S}'.format(datetime.datetime.now())) #111657
+    f.write('{:,%H%M%S}'.format(datetime.datetime.now())) #111657
     f.write(',202284220170504111657,1,,,,,,,,')
     f.write('Sanku Sinha') #Sanku Sinha
     f.write(',,,,,,,,,')
